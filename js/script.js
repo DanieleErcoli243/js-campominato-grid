5c8446324a9c12a994c8b3539521eccab7629558
Polvere243 <danielercoli@gmail.com>
2024-01-10 20:58:28 +0100

    created 100 cells and appended them to the grid

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ const levelChoice = document.getElementById("level");
 const grid = document.querySelector(".grid");
 
 // dichiaro la costante col numero totale di celle
-const totalcells = 100;
+const totalCells = 100;
 
+// aggancio la creazione della griglia al click del pulsante
 
+button.addEventListener("click", function () {
+    for (let i = 0; i < totalCells; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        grid.appendChild(cell);
+    }
+})
