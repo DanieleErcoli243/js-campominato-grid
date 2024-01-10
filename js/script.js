eff1266ac29c39e01899dc8d62041c7334d74622
Polvere243 <danielercoli@gmail.com>
2024-01-10 21:54:55 +0100

    added some comments

--- js/script.js
+++ js/script.js
@@ -9,12 +9,15 @@ const totalCells = 100;
 // aggancio la creazione della griglia al click del pulsante
 
 button.addEventListener("click", function () {
+    // creo un elemento colla classe cell per 100 volte
     for (let i = 0; i < totalCells; i++){
         const cell = document.createElement("div");
+        // metto nella cella il numero che le corrisponde
         cell.innerText = i + 1;
         cell.classList.add("cell");
+        // appendo le celle alla griglia
         grid.appendChild(cell);
-
+        // aggancio al click sulle celle il toggle della classe clicked per colorare le suddette
         cell.addEventListener("click", function () {
             cell.classList.toggle("clicked");
             console.log(i + 1);
