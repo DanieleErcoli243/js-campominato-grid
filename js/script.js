44e26180883d7dde9854beca05a29274bbeab306
Polvere243 <danielercoli@gmail.com>
2024-01-10 20:38:50 +0100

    got elements from the DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log("JS OK");
+// recupero gli elementi dal DOM
+const button = document.getElementById("play");
+const levelChoice = document.getElementById("level");
+const grid = document.querySelector(".grid");
