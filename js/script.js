eb8a2e06f0e0db4f11b0c60561304a7a538903e7
Polvere243 <danielercoli@gmail.com>
2024-01-10 20:49:41 +0100

    created a variable for the number of cells to create

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 const button = document.getElementById("play");
 const levelChoice = document.getElementById("level");
 const grid = document.querySelector(".grid");
+
+// dichiaro la costante col numero totale di celle
+const totalcells = 100;
+
+
