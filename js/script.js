42cf104eea5fa88ad08d6614279e05a6863919af
Polvere243 <danielercoli@gmail.com>
2024-01-15 01:28:18 +0100

    broke everything applying corrections

--- js/script.js
+++ js/script.js
@@ -2,30 +2,64 @@
 const button = document.getElementById("play");
 const levelChoice = document.getElementById("level");
 const grid = document.querySelector(".grid");
-const bigLevel = document.getElementById("big");
-const mediumLevel = document.getElementById("medium");
-const smallLevel = document.getElementById("small");
-// dichiaro la costante col numero totale di cell
-const totalCells = 100;
+const form = document.querySelector("form");
+// dichiaro le costanti per determinare il numero totale di celle
+
+let rows = 10;
+let cols = 10;
+const totalCells = rows * cols;
+
+// funzioni
+const createCell = (cellNumber, levelclass) => {
+    // creo una cella con dentro il numero corrispondente
+    const cell = document.createElement("div");
+    // metto nella cella il numero che le corrisponde
+    cell.innerText = cellNumber +1;
+    // aggiungo la classe cel per stilizzare le celle
+    cell.classList.add("cell", levelclass);
+    // restituisco la cella
+    return cell;
+}
 
 // aggancio la creazione della griglia al click del pulsante
 
-button.addEventListener("click", function () {
-    // creo un elemento colla classe cell per 100 volte
+form.addEventListener("submit", function (e) {
+    // funzioni interne al gioco
+    e.preventDefault();
+    // cambio il testo al bottone
+    button.innerText = "Rigioca";
+    // svuoto la griglia
+    grid.innerHTML = "";
+    console.log("cliccato");
+    // recupero la scelta di livello dell'utente
+    const level = levelChoice.value;
+    // determino il livello aggiungendo una classe alla griglia
+    grid.classList.add(level);
+    switch (level) {
+        case "medium" : 
+            rows = 9;
+            cols = 9;  
+            break;
+
+        case "small" :
+            rows = 7;
+            cols = 7;
+            break;
+    }
+
+    // creo un elemento colla classe cell per il prodotto di righe e colonne
     for (let i = 0; i < totalCells; i++){
-        const cell = document.createElement("div");
-        // metto nella cella il numero che le corrisponde
-        cell.innerText = i + 1;
-        cell.classList.add("cell");
+        // invoco la funzione per generare le celle dinamicamente
+        const cell = createCell (i);
         // appendo le celle alla griglia
         grid.appendChild(cell);
         // aggancio al click sulle celle il toggle della classe clicked per colorare le suddette
-        cell.addEventListener("click", function () {
-            cell.classList.toggle("clicked");
+        cell.addEventListener("click", (event) => {
+            if (this.classList.contains("clicked")) return;
+            cell.classList.add("clicked");
             console.log(i + 1);
         })
     }
-    
     
 })
 
