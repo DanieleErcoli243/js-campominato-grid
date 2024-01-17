34e7e6937c3e36071ad7a3cf2f496a8a3e8b7b66
Polvere243 <danielercoli@gmail.com>
2024-01-17 02:16:39 +0100

    failed to change grid size by using the switch

--- js/script.js
+++ js/script.js
@@ -1,72 +1,76 @@
 // recupero gli elementi dal DOM
 const button = document.getElementById("play");
 const levelChoice = document.getElementById("level");
-const grid = document.querySelector(".grid");
+const grid = document.getElementById("grid");
 const form = document.querySelector("form");
 // dichiaro le costanti per determinare il numero totale di celle
 
-let rows = 10;
-let cols = 10;
-const totalCells = rows * cols;
+
+
 
 // funzioni
-const createCell = (cellNumber, levelclass) => {
-    // creo una cella con dentro il numero corrispondente
-    const cell = document.createElement("div");
-    // metto nella cella il numero che le corrisponde
-    cell.innerText = cellNumber +1;
-    // aggiungo la classe cel per stilizzare le celle
-    cell.classList.add("cell", levelclass);
-    // restituisco la cella
-    return cell;
-}
+// dichiaro una funzione per generare le celle
 
-// aggancio la creazione della griglia al click del pulsante
-
-form.addEventListener("submit", function (e) {
-    // funzioni interne al gioco
-    e.preventDefault();
-    // cambio il testo al bottone
-    button.innerText = "Rigioca";
+const startGame = event => {
+    event.preventDefault();
     // svuoto la griglia
-    grid.innerHTML = "";
-    console.log("cliccato");
-    // recupero la scelta di livello dell'utente
-    const level = levelChoice.value;
-    // determino il livello aggiungendo una classe alla griglia
-    grid.classList.add(level);
-    switch (level) {
-        case "medium" : 
-            rows = 9;
-            cols = 9;  
-            break;
+    grid.innerText = "";
+    // funzioni nel gioco
+    // funzione che genera la cella
+    const createCell = (cellNumber) => {
+        // creo la cella la stilizzo colla classe cell
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        // metto nella cella il numero che le corrisponde
+        cell.append (cellNumber + 1);
+        return cell;
+    }
+    
 
-        case "small" :
+    // funzione che cambia il colore della cella ed esegue il console.log del numero della cella 
+    const onCellClicked = event => {
+        // impedisco di ricliccare una cella
+        if (cell.classList.containes("clicked")) return;
+        cell.classList.add("clicked");
+        console.log(event.target.innertText);
+    }
+    // cambio il testo nel bottone
+    button.innerText = "Rigioca";
+    
+    // recupero i valori dei livelli
+    const level = levelChoice.value;
+    // determino la grandezza delle celle in base al valore
+    grid.classList.add(level);
+    // determino la quantità di celle in base al value
+    let rows;
+    let cols;
+    const totalCells = rows * cols;
+    switch (level) {
+        case "medium":
+            rows = 9;
+            cols = 9;
+            break;
+        case "small":
             rows = 7;
             cols = 7;
             break;
+        default:
+            rows = 10;
+            cols = 10;
     }
-
-    // creo un elemento colla classe cell per il prodotto di righe e colonne
+    console.log(totalCells);
+    // genero le celle
     for (let i = 0; i < totalCells; i++){
-        // invoco la funzione per generare le celle dinamicamente
-        const cell = createCell (i);
+        const cell = createCell(i);
+        
         // appendo le celle alla griglia
         grid.appendChild(cell);
         // aggancio al click sulle celle il toggle della classe clicked per colorare le suddette
-        cell.addEventListener("click", (event) => {
-            if (this.classList.contains("clicked")) return;
-            cell.classList.add("clicked");
-            console.log(i + 1);
-        })
+        cell.addEventListener("click", onCellClicked);
     }
-    
-})
 
+    // cell.addEventListener ("click", onCellCLicked)
+}
 
-
-levelChoice.addEventListener ("change", function () {
-    const levelValue = levelChoice.value;
-    console.log(levelValue);
-})
+form.addEventListener("submit", startGame);
 
